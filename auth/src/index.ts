c3353c205a25d876b871c64f2b8c811bac07f2b5
Oleksandr_Amirov <xrocketmanx007@gmail.com>
2021-12-13 18:25:17 +0200

    change message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -12,7 +12,7 @@ const start = async () => {
     }
 
     app.listen(3000, () => {
-        console.log('Auth service listening on port 3000!');
+        console.log('Auth service listening on port 3000!!');
     });
 };
 
